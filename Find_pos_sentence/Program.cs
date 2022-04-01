3ec4546973ea3e56784e4c82a5b4b2af3b4bb95d
suhendhar02 <suhendhar02@HP>
2022-04-01 20:05:34 +0530

    Add project files.

---
+++ Find_pos_sentence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_pos_sentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "The quick brown fox box jumps over the lazy dog.";
+            Console.WriteLine("Original string:"+sentence);
+            Console.WriteLine("Position of the word 'lazy' in the said string:"+test(sentence,"lazy"));
+        }
+        public static int test(string text, string word)
+        { return Array.IndexOf(text.Split(' '), word) + 1; }
+    }
+}
